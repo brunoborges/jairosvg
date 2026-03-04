d48967fcb79ac89b6cc9ad08c1a9593a40ebbff8
Bruno Borges <brborges@microsoft.com>
2026-03-04 00:03:19 -0500

    Bump version to 1.0.1

--- src/main/java/io/brunoborges/jairosvg/JairoSVG.java
+++ src/main/java/io/brunoborges/jairosvg/JairoSVG.java
@@ -31,7 +31,7 @@ import io.brunoborges.jairosvg.surface.*;
  */
 public final class JairoSVG {
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     private JairoSVG() {
     }
